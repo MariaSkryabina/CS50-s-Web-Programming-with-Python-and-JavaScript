73865d80acfa4653ae1493eb1c15ed14888324f8
Maria Skryabina <skryabina.mary98@gmail.com>
2022-10-13 16:51:16 +0300

    basic realisation of isitchristmas.com is working

---
+++ PROJECT_NAME/APP_NAME/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, "APP_NAME/index.html")
+
+def brian(request):
+    return HttpResponse("Hello, Brian!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+def greet(request, name):
+    return render(request, "APP_NAME/greet.html", {
+        "name": name.capitalize()
+    })
