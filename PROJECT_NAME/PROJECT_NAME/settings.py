9cf71b12ba4d5c03e348233ffdf9f06c5d2c8db6
Maria Skryabina <skryabina.mary98@gmail.com>
2022-10-13 17:53:39 +0300

    new file .env for secret key is created. Added gitignore

--- PROJECT_NAME/PROJECT_NAME/settings.py
+++ PROJECT_NAME/PROJECT_NAME/settings.py
@@ -9,18 +9,23 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&wgw$r&a3yzn-9j9g%m=5=fe==c9iaffa9ypfsu08jmxncw+5*'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
